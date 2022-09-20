5b55292dcb88e652be626292b4f2623f470c67b6
LySeriuos <drungilas.p@gmail.com>
2022-09-20 11:56:56 +0200

    Add project files.

---
+++ Project NR.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace floorSpaceCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter floors width in meters:");
+            decimal floorWidth = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter floors length in meters:");
+            decimal floorLength = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter floors price per sqm:");
+            decimal floorPrice = Convert.ToDecimal(Console.ReadLine());
+
+            decimal floorAmount = ((decimal)floorWidth * (decimal)floorLength);
+
+            decimal floorTotal = ((decimal)floorAmount * (decimal)floorPrice);
+
+            Console.WriteLine($"Floor area is: {floorAmount} and \nthe price for the floor is: {floorTotal}EUR");
+        }
+    }
+}
